20ff5e721c7e7e5afff4e906ee0f6faf4932568f
lannle <le_l1@denison.edu>
2018-01-16 23:23:29 -0800

    get data from Google Places API

--- page/ExploreCityPage.js
+++ page/ExploreCityPage.js
@@ -1,12 +1,28 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Button} from 'react-native';
+import axios from 'axios'
+
+
+const info = require('../info.json');
+
+
 
 
 export default class ExploreCityPage extends React.Component {
+
+	getPlaces(){
+		axios.get('https://maps.googleapis.com/maps/api/place/textsearch/json?query=point_of_interest+in+Sydney&key='+info.API_KEY).then((response)=>{
+			console.log(response.data.results)
+		})
+	}
 	render() {
 	     return (
 	     	<View>
-	     		<Text>Explore all those cool places in this city </Text>
+		     	<Button
+					  onPress={this.getPlaces}
+					  title="Press Me"
+					  color="#841584"
+				/>
 
 	     	</View>
 	     )
